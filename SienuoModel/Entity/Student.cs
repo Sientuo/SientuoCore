3f1c222be77b3066adc75fd8c2f4a75ccb04d115
Sientuo <kiaser@foxmail.com>
2024-03-10 10:20:19 +0800

    实体注释

--- SienuoModel/Entity/Student.cs
+++ SienuoModel/Entity/Student.cs
@@ -8,17 +8,23 @@ namespace SienuoModel
 {
     public class Student
     {
+        //编号
         public  string Id { get; set; }
 
+        //用户编号
         public string SUserCode { get; set; }
-
+        //用户姓名
         public string SName { get; set; }
+        //班级编号
         public string ClassId { get; set; }
+        //性别
         public int Sex { get; set; }
         public string CreateBy { get; set; }
 
+        //密码
         public string SPassword { get; set; }
 
+        //创建时间
         public DateTime CreateDate { get; set; }
 
     }
